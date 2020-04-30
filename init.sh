8a720f6df7d5f29bb3f3e938ba2091121f74cb8c
Yu <sekimoto.y@dentsudigital.co.jp>
2020-04-30 22:04:49 +0900

    fixed typo

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ for i in `seq 164`
 do
   zero_padding=`printf %03d $i`
   mkdir ./recents/ABC_${zero_padding}
-  if [ $i -gt 126 ]; then
+  if [ $i -ge 126 ]; then
     touch ./recents/ABC_${zero_padding}/{A,B,C,D,E,F}.py
   else
     touch ./recents/ABC_${zero_padding}/{A,B,C,D}.py
