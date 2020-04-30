0cd2e08ddb9c9f2e213ee0bb89b501d49b26f79b
Yu <sekimoto.y@dentsudigital.co.jp>
2020-04-30 21:58:22 +0900

    modified init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-for i in `seq 162`
+for i in `seq 164`
 do
   zero_padding=`printf %03d $i`
-  mkdir ./recents/ABC_${zero_padding} && touch ./recents/ABC_${zero_padding}/{A,B,C,D,E,F}.py
+  mkdir ./recents/ABC_${zero_padding}
+  if [ $i -gt 126 ]; then
+    touch ./recents/ABC_${zero_padding}/{A,B,C,D,E,F}.py
+  else
+    touch ./recents/ABC_${zero_padding}/{A,B,C,D}.py
+  fi
 done
